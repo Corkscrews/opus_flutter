be469db54a67f8df2b028a2c15f3497689fb58e4
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2026-02-25 01:46:59 +0000

    refactor: deduplicate OpusPacketUtils with shared _withNativePacket helper

--- opus_dart/lib/src/opus_dart_packet.dart
+++ opus_dart/lib/src/opus_dart_packet.dart
@@ -3,87 +3,58 @@ import 'dart:typed_data';
 import '../wrappers/opus_defines.dart' as opus_defines;
 import 'opus_dart_misc.dart';
 
-/// Bundles utility functions to examin opus packets.
+/// Bundles utility functions to examine opus packets.
 ///
 /// All methods copy the input data into native memory.
 abstract class OpusPacketUtils {
-  /// Returns the amount of samples in a [packet] given a [sampleRate].
-  static int getSampleCount(
-      {required Uint8List packet, required int sampleRate}) {
+  static int _withNativePacket(
+      Uint8List packet, int Function(Pointer<Uint8> data) operation) {
     Pointer<Uint8> data = opus.allocator.call<Uint8>(packet.length);
     data.asTypedList(packet.length).setAll(0, packet);
     try {
-      int sampleCount = opus.decoder
-          .opus_packet_get_nb_samples(data, packet.length, sampleRate);
-      if (sampleCount < opus_defines.OPUS_OK) {
-        throw OpusException(sampleCount);
+      int result = operation(data);
+      if (result < opus_defines.OPUS_OK) {
+        throw OpusException(result);
       }
-      return sampleCount;
+      return result;
     } finally {
       opus.allocator.free(data);
     }
   }
 
+  /// Returns the amount of samples in a [packet] given a [sampleRate].
+  static int getSampleCount(
+      {required Uint8List packet, required int sampleRate}) {
+    return _withNativePacket(
+        packet,
+        (data) => opus.decoder
+            .opus_packet_get_nb_samples(data, packet.length, sampleRate));
+  }
+
   /// Returns the amount of frames in a [packet].
   static int getFrameCount({required Uint8List packet}) {
-    Pointer<Uint8> data = opus.allocator.call<Uint8>(packet.length);
-    data.asTypedList(packet.length).setAll(0, packet);
-    try {
-      int frameCount =
-          opus.decoder.opus_packet_get_nb_frames(data, packet.length);
-      if (frameCount < opus_defines.OPUS_OK) {
-        throw OpusException(frameCount);
-      }
-      return frameCount;
-    } finally {
-      opus.allocator.free(data);
-    }
+    return _withNativePacket(packet,
+        (data) => opus.decoder.opus_packet_get_nb_frames(data, packet.length));
   }
 
   /// Returns the amount of samples per frame in a [packet] given a [sampleRate].
   static int getSamplesPerFrame(
       {required Uint8List packet, required int sampleRate}) {
-    Pointer<Uint8> data = opus.allocator.call<Uint8>(packet.length);
-    data.asTypedList(packet.length).setAll(0, packet);
-    try {
-      int samplesPerFrame =
-          opus.decoder.opus_packet_get_samples_per_frame(data, sampleRate);
-      if (samplesPerFrame < opus_defines.OPUS_OK) {
-        throw OpusException(samplesPerFrame);
-      }
-      return samplesPerFrame;
-    } finally {
-      opus.allocator.free(data);
-    }
+    return _withNativePacket(
+        packet,
+        (data) =>
+            opus.decoder.opus_packet_get_samples_per_frame(data, sampleRate));
   }
 
   /// Returns the channel count from a [packet]
   static int getChannelCount({required Uint8List packet}) {
-    Pointer<Uint8> data = opus.allocator.call<Uint8>(packet.length);
-    data.asTypedList(packet.length).setAll(0, packet);
-    try {
-      int channelCount = opus.decoder.opus_packet_get_nb_channels(data);
-      if (channelCount < opus_defines.OPUS_OK) {
-        throw OpusException(channelCount);
-      }
-      return channelCount;
-    } finally {
-      opus.allocator.free(data);
-    }
+    return _withNativePacket(
+        packet, (data) => opus.decoder.opus_packet_get_nb_channels(data));
   }
 
   /// Returns the bandwidth from a [packet]
   static int getBandwidth({required Uint8List packet}) {
-    Pointer<Uint8> data = opus.allocator.call<Uint8>(packet.length);
-    data.asTypedList(packet.length).setAll(0, packet);
-    try {
-      int bandwidth = opus.decoder.opus_packet_get_bandwidth(data);
-      if (bandwidth < opus_defines.OPUS_OK) {
-        throw OpusException(bandwidth);
-      }
-      return bandwidth;
-    } finally {
-      opus.allocator.free(data);
-    }
+    return _withNativePacket(
+        packet, (data) => opus.decoder.opus_packet_get_bandwidth(data));
   }
 }
